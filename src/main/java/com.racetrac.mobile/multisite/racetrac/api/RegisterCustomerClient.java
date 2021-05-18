5dcbbdefeb6a4783210896cee7192736ed643d22
Serhii_Suzanskyi <Serhii_Suzanskyi@epam.com>
2021-05-18 12:43:07 +0300

    added Required header X-Platform to all of requests

--- src/main/java/com.racetrac.mobile/multisite/racetrac/api/RegisterCustomerClient.java
+++ src/main/java/com.racetrac.mobile/multisite/racetrac/api/RegisterCustomerClient.java
@@ -2,21 +2,20 @@ package com.racetrac.mobile.multisite.racetrac.api;
 
 import com.racetrac.mobile.multisite.racetrac.dto.CustomerDto;
 import okhttp3.Request;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static com.racetrac.mobile.multisite.racetrac.api.UrlUtils.API_ENDPOINT;
-import static com.racetrac.mobile.multisite.racetrac.api.UrlUtils.API_SUBSCRIPTION_KEY;
-import static com.racetrac.mobile.multisite.racetrac.api.UrlUtils.API_USERS;
+import static com.racetrac.mobile.multisite.racetrac.api.UrlUtils.API_URL;
+import static com.racetrac.mobile.multisite.racetrac.api.UrlUtils.API_USERS_ENDPOINT;
 
 @Component
 public class RegisterCustomerClient extends HttpClient {
 
     public CustomerDto registerAccount(CustomerDto customerDto) {
         Request registerRequest = new Request.Builder()
-                .url(API_ENDPOINT+API_USERS)
-                .addHeader("Ocp-Apim-Subscription-Key", API_SUBSCRIPTION_KEY)
-               // .addHeader("Content-Type", "application/json")
+                .url(API_URL + API_USERS_ENDPOINT)
+//                .addHeader("Ocp-Apim-Subscription-Key", API_SUBSCRIPTION_KEY)
+                // .addHeader("Content-Type", "application/json")
+                .headers(getRequiredHttpHeaders())
                 .post(createRequestBody(getGson().toJson(customerDto)))
                 .build();
         checkSuccessResponse(registerRequest);
