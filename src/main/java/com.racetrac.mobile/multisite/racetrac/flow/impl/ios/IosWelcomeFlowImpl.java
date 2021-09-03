09a0f0e5d29d2121900c3ab0c22d638f8358a776
Serhii_Suzanskyi <Serhii_Suzanskyi@epam.com>
2021-09-03 12:44:16 +0300

    [Not Authorised] added ios part to points and tears tests test

--- src/main/java/com.racetrac.mobile/multisite/racetrac/flow/impl/ios/IosWelcomeFlowImpl.java
+++ src/main/java/com.racetrac.mobile/multisite/racetrac/flow/impl/ios/IosWelcomeFlowImpl.java
@@ -3,6 +3,7 @@ package com.racetrac.mobile.multisite.racetrac.flow.impl.ios;
 import com.racetrac.mobile.multisite.racetrac.flow.BaseFlow;
 import com.racetrac.mobile.multisite.racetrac.flow.WelcomeFlow;
 import io.qameta.allure.Step;
+import org.openqa.selenium.NoSuchElementException;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -25,16 +26,20 @@ public class IosWelcomeFlowImpl extends BaseFlow implements WelcomeFlow {
 
     @Override
     public boolean isHeroLevelHelpBtnDisplayed() {
-        return false;
+        try {
+            return getHomePage().getHeroLevelHelpBtn().isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     @Override
     public boolean isRedeemPointsIsDisplayed() {
-        return false;
+        return getHomePage().getRedeemPointsBtn().isDisplayed();
     }
 
     @Override
     public void clickRedeemPointsBtn() {
-
+        getHomePage().getRedeemPointsBtn().click();
     }
 }
