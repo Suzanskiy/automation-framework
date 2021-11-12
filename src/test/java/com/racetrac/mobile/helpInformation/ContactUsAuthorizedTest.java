6dc6ca3dec6f18002cf96fc35a2ad8e9742ff90c
Serhii_Suzanskyi <Serhii_Suzanskyi@epam.com>
2021-11-12 11:02:11 +0200

    enabled Connection for other tests
    Added error message check, edited description
    
    updated error message handling

--- src/test/java/com/racetrac/mobile/helpInformation/ContactUsAuthorizedTest.java
+++ src/test/java/com/racetrac/mobile/helpInformation/ContactUsAuthorizedTest.java
@@ -2,7 +2,14 @@ package com.racetrac.mobile.helpInformation;
 
 import com.racetrac.mobile.BaseTest;
 import com.racetrac.mobile.multisite.racetrac.dto.CustomerDto;
-import com.racetrac.mobile.multisite.racetrac.flow.*;
+import com.racetrac.mobile.multisite.racetrac.flow.AccountSettingsFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.ContactUsFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.HelpInformationFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.NoInternetConnectionFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.SignInFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.SignUpFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.SuccessfulSentContactUsMessageFlow;
+import com.racetrac.mobile.multisite.racetrac.flow.WelcomeFlow;
 import com.racetrac.mobile.util.constants.TestData;
 import com.racetrac.mobile.util.device.ConnectionUtils;
 import io.qameta.allure.Description;
@@ -12,13 +19,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.racetrac.mobile.util.device.ConnectionUtils.*;
+import static com.racetrac.mobile.util.device.ConnectionUtils.disableConnection;
+import static com.racetrac.mobile.util.device.ConnectionUtils.enableConnection;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
 
 public class ContactUsAuthorizedTest extends BaseTest {
 
     private static final String TEST_MESSAGE = "This is test message from QA team";
     private static final String PATH = "testData/text5000symbols.txt";
+    public static final String FAILED_SUBMISSION_MESSAGE = "Your submission was not successful. Please try again.";
 
     @Autowired
     WelcomeFlow welcomeFlow;
@@ -34,6 +44,8 @@ public class ContactUsAuthorizedTest extends BaseTest {
     SuccessfulSentContactUsMessageFlow successfulSentContactUsMessageFlow;
     @Autowired
     NoInternetConnectionFlow noInternetConnectionFlow;
+    @Autowired
+    SignUpFlow signUpFlow;
 
     CustomerDto customerDto;
 
@@ -66,15 +78,17 @@ public class ContactUsAuthorizedTest extends BaseTest {
     }
 
     @TmsLink("8826")
-    @Description("Information about a successfully sent message in \"Contact Us\" is displayed when the Internet is disconnected")
+    @Description("Message in \"Contact Us\" can't be sent when the Internet is disconnected")
     @Test
     public void contactUsAuthorizedUserWithoutInternetTest() {
         assertTrue(contactUsFlow.isContactUsPageOpened(), "Contact us Page is not opened");
         disableConnection();
-        assertTrue(noInternetConnectionFlow.isNoInternetConnectionPageOpened(),"No Internet connection page is not opened");
+        assertTrue(noInternetConnectionFlow.isNoInternetConnectionPageOpened(), "No Internet connection page is not opened");
         noInternetConnectionFlow.tapOkBtn();
         contactUsFlow.sendMessageToGeneralSubject(TEST_MESSAGE);
-        assertTrue(successfulSentContactUsMessageFlow.isSuccessfulSentContactUsMessageOpened(), "The page after the sent message is not opened");
+        assertTrue(signUpFlow.isErrorMessageShown());
+        assertEquals(signUpFlow.getErrorMessageText(), FAILED_SUBMISSION_MESSAGE);
+        enableConnection();
     }
 
     @TmsLink("8826")
@@ -88,7 +102,7 @@ public class ContactUsAuthorizedTest extends BaseTest {
         assertTrue(helpInformationFlow.isHelpInformationPageOpened(), "Help information page is not opened");
         helpInformationFlow.navigateToContactUs();
         assertTrue(contactUsFlow.isContactUsPageOpened(), "Contact us Page is not opened");
-        contactUsFlow.sendMessageToGeneralSubject(TestData.readFromFile(PATH)+"A");
+        contactUsFlow.sendMessageToGeneralSubject(TestData.readFromFile(PATH) + "A");
         assertTrue(successfulSentContactUsMessageFlow.isSuccessfulSentContactUsMessageOpened(), "The page after the sent message is not opened");
     }
 
