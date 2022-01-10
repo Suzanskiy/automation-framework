245ea33021c2d4e5a5269858910b093355834353
Serhii_Suzanskyi <Serhii_Suzanskyi@epam.com>
2022-01-10 16:02:17 +0200

    added ios part

--- src/main/java/com.racetrac.mobile/multisite/racetrac/flow/impl/ios/iosFacebookAuthFlowImpl.java
+++ src/main/java/com.racetrac.mobile/multisite/racetrac/flow/impl/ios/iosFacebookAuthFlowImpl.java
@@ -4,29 +4,39 @@ import com.racetrac.mobile.multisite.racetrac.dto.CustomerDto;
 import com.racetrac.mobile.multisite.racetrac.flow.BaseFlow;
 import com.racetrac.mobile.multisite.racetrac.flow.FacebookAuthFlow;
 import com.racetrac.mobile.util.device.AlertUtil;
+import io.qameta.allure.Step;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import static com.racetrac.mobile.framework.constants.PlatformsConstants.ANDROID;
 import static com.racetrac.mobile.framework.constants.PlatformsConstants.IOS;
+import static com.racetrac.mobile.util.appium.AppiumWaitingUtils.waitUntilElementClickable;
 
 @Profile(IOS)
 @Component
 public class iosFacebookAuthFlowImpl extends BaseFlow implements FacebookAuthFlow {
 
-    @Override
-    public void authorise(final CustomerDto customerDto) {
-        AlertUtil.acceptAlert();
-        getFacebookLoginPage().waitUntilIsOpened();
-        getFacebookLoginPage().getEmailInput().clear();
-        getFacebookLoginPage().getEmailInput().sendKeys(customerDto.getPersonalInfo().getEmail());
-        getFacebookLoginPage().getPassInput().sendKeys(customerDto.getEmailAuth().getPassword());
-        getFacebookLoginPage().getLoginBtn().click();
-        getFacebookLoginStep2Page().waitUntilIsOpened();
-        getFacebookLoginStep2Page().getContinueBtn().click();
-    }
+  @Override
+  public void authorise(final CustomerDto customerDto) {
+    AlertUtil.acceptAlert();
+    getFacebookLoginPage().waitUntilIsOpened();
+    getFacebookLoginPage().getEmailInput().clear();
+    getFacebookLoginPage().getEmailInput().sendKeys(customerDto.getPersonalInfo().getEmail());
+    getFacebookLoginPage().getPassInput().sendKeys(customerDto.getEmailAuth().getPassword());
+    getFacebookLoginPage().getLoginBtn().click();
+    getFacebookLoginStep2Page().waitUntilIsOpened();
+    getFacebookLoginStep2Page().getContinueBtn().click();
+  }
 
-    @Override
-    public void singInToFacebook() {
-    }
+  @Step
+  @Override
+  public void singInToFacebook() {
+    clickContinueBtn();
+  }
+
+  private void clickContinueBtn() {
+    AlertUtil.acceptAlert();
+    getFacebookLoginStep2Page().waitUntilIsOpened();
+    waitUntilElementClickable(getFacebookLoginStep2Page().getContinueBtn());
+    getFacebookLoginStep2Page().getContinueBtn().click();
+  }
 }
