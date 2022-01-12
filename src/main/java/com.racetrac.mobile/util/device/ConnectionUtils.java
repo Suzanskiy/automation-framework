2fac8719e88207a6e7c9d7756bc029bcce1ca538
IhorNikolaiev <87424194+IhorNikolaiev@users.noreply.github.com>
2022-01-12 12:46:36 +0200

    ConnectionUtils checking the internet

--- src/main/java/com.racetrac.mobile/util/device/ConnectionUtils.java
+++ src/main/java/com.racetrac.mobile/util/device/ConnectionUtils.java
@@ -1,11 +1,12 @@
 package com.racetrac.mobile.util.device;
 
+import com.racetrac.mobile.util.appium.Action;
+import com.racetrac.mobile.util.appium.AppiumWaitingUtils;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.connection.ConnectionStateBuilder;
 import io.qameta.allure.Step;
 
 import static com.racetrac.mobile.util.appium.AppiumDriverUtils.getDriver;
-
 
 public class ConnectionUtils {
     static AndroidDriver androidDriver = (AndroidDriver) getDriver();
@@ -16,6 +17,14 @@ public class ConnectionUtils {
                 .withWiFiDisabled()
                 .withDataDisabled()
                 .build());
+
+        Action action = () -> {
+            while (androidDriver.getConnection().isWiFiEnabled()&&androidDriver.getConnection().isDataEnabled()) {
+                return false;
+            }
+            return true;
+        };
+        AppiumWaitingUtils.waitUntilIsTrue(action);
     }
 
     @Step
@@ -24,6 +33,13 @@ public class ConnectionUtils {
                 .withWiFiEnabled()
                 .withDataEnabled()
                 .build());
-    }
 
+        Action action = () -> {
+            while (!(androidDriver.getConnection().isWiFiEnabled()&&androidDriver.getConnection().isDataEnabled())) {
+                return false;
+            }
+            return true;
+        };
+        AppiumWaitingUtils.waitUntilIsTrue(action);
+    }
 }
